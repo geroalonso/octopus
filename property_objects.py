2e0701dddfe940fb128c6803ccf1af3a210b457a
geroalonso <geronimoalonso@icloud.com>
2021-03-12 17:14:01 -0300

    Update property_objects.py

--- property_objects.py
+++ property_objects.py
@@ -5,11 +5,11 @@ class Property:
 		self.address = address
 		self.county = county
 
-biscayne = Property('Total Biscayne Corp', 6.5,'12955 Biscayne Boulevard, Suite 406B, North Miami, FL 33181','Miami Dade')
+biscayne = Property('Total Biscayne Corp', 6.5,'12955 Biscayne Boulevard, North Miami, FL 33181','Miami Dade')
 doral_cuerpo_1 = Property('Doral Flex One LLC', 6.5, '3131 North West 79th Avenue, Doral, FL 33122', 'Miami Dade')
 doral_cuerpo_2 = Property('Doral Flex One LLC', 6.5, '7850 North West 32nd Street, Doral, FL 33122', 'Miami Dade')
 plantation = Property('Plantation One LLC', 6.5, '377 North State Road 7, Plantation, FL 33317', 'Broward')
 sample = Property('Sample Usa Corp', 6.5, '7355-7679 West Sample Road, Coral Springs, FL 33065', 'Broward')
-oaktree = Property('Iberic Group Corp', 6.5, '11575-11585 US Highway 1, North Miami, FL 33408', 'Broward' )
+oaktree = Property('Iberic Group Corp', 6.5, '11575-11585 US Highway 1, North Palm Beach, FL 33408', 'Broward' )
 hiawassee = Property('New Orlando Team Corp', 6.5, '6801-6909 West Colonial Drive, Orlando, FL 32818', 'Orange')
 portrichey = Property('New Florida Team Corp', 6.5, '6538-6670 Ridge Road, Port Richey, FL 34668','Pasco')
