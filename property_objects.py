c73b423da5d1c72aec6abcb309f0426b9df0bf92
Geronimo Alonso <geronimoalonso@icloud.com>
2021-05-25 10:57:49 -0300

    adding royal plaza

--- property_objects.py
+++ property_objects.py
@@ -10,6 +10,7 @@ doral_cuerpo_1 = Property('Doral Flex One LLC', 6.5, '3131 North West 79th Avenu
 doral_cuerpo_2 = Property('Doral Flex One LLC', 6.5, '7850 North West 32nd Street, Doral, FL 33122', 'Miami Dade')
 plantation = Property('Plantation One LLC', 6.5, '377 North State Road 7, Plantation, FL 33317', 'Broward')
 sample = Property('Sample Usa Corp', 6.5, '7355-7679 West Sample Road, Coral Springs, FL 33065', 'Broward')
+royal = Property('Yellowbird International Corp', 6.5, '601-650 Royal Palm Beach Blvd, Royal Palm Beach, FL 33411', 'Broward')
 oaktree = Property('Iberic Group Corp', 6.5, '11575-11585 US Highway 1, North Palm Beach, FL 33408', 'Broward' )
 hiawassee = Property('New Orlando Team Corp', 6.5, '6801-6909 West Colonial Drive, Orlando, FL 32818', 'Orange')
 portrichey = Property('New Florida Team Corp', 6.5, '6538-6670 Ridge Road, Port Richey, FL 34668','Pasco')
